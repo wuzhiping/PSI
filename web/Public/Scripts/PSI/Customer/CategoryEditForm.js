99afb890c5db7cd55f70d47478e4e9c77baec537
CRM8000 <crm8000@qq.com>
2021-07-05 14:54:25 +0800

    客户

--- web/Public/Scripts/PSI/Customer/CategoryEditForm.js
+++ web/Public/Scripts/PSI/Customer/CategoryEditForm.js
@@ -43,19 +43,8 @@ Ext.define("PSI.Customer.CategoryEditForm", {
       fields: ["id", "name"]
     });
 
-    var t = entity == null ? "新增客户分类" : "编辑客户分类";
-    var f = entity == null
-      ? "edit-form-create.png"
-      : "edit-form-update.png";
-    var logoHtml = "<img style='float:left;margin:10px 20px 0px 10px;width:48px;height:48px;' src='"
-      + PSI.Const.BASE_URL
-      + "Public/Images/"
-      + f
-      + "'></img>"
-      + "<h2 style='color:#196d83'>"
-      + t
-      + "</h2>"
-      + "<p style='color:#196d83'>标记 <span style='color:red;font-weight:bold'>*</span>的是必须录入数据的字段</p>";
+    var t = entity == null ? "新建客户分类" : "编辑客户分类";
+    var logoHtml = me.genLogoHtml(entity, t);
 
     Ext.apply(me, {
       header: {
@@ -68,7 +57,7 @@ Ext.define("PSI.Customer.CategoryEditForm", {
       items: [{
         region: "north",
         border: 0,
-        height: 90,
+        height: 70,
         html: logoHtml
       }, {
         region: "center",
