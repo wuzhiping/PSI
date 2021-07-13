7e38b48df8da8148da4af08fbf13ed16ea555917
CRM8000 <crm8000@qq.com>
2021-07-13 10:33:36 +0800

    存货拆分

--- web/Public/Scripts/PSI/WSP/WSPMainForm.js
+++ web/Public/Scripts/PSI/WSP/WSPMainForm.js
@@ -1004,9 +1004,8 @@ Ext.define("PSI.WSP.WSPMainForm", {
           if (success) {
             var data = me.decodeJSON(response.responseText);
             if (data.success) {
-              me.showInfo("成功完成提交操作", function () {
-                me.refreshMainGrid(data.id);
-              });
+              me.refreshMainGrid(data.id);
+              me.tip("成功完成提交操作");
             } else {
               me.showInfo(data.msg);
             }
