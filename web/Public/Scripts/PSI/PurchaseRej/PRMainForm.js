bf3cf13e82d57cfbe3dbeb7b28d1e6c52ff97a2f
CRM8000 <crm8000@qq.com>
2021-07-13 09:33:53 +0800

    采购退货出库

--- web/Public/Scripts/PSI/PurchaseRej/PRMainForm.js
+++ web/Public/Scripts/PSI/PurchaseRej/PRMainForm.js
@@ -121,8 +121,9 @@ Ext.define("PSI.PurchaseRej.PRMainForm", {
       xtype: "tbseparator",
       hidden: me.getPermission().print == "0"
     }, {
-      text: "帮助",
+      text: "指南",
       handler: function () {
+        me.focus();
         window.open(me.URL("Home/Help/index?t=prbill"));
       }
     }, "-", {
