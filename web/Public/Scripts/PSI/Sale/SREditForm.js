1ece36ec56b5c9b6e0e94e7210b136c85fc1216f
CRM8000 <crm8000@qq.com>
2021-07-15 14:55:33 +0800

    销售退货入库

--- web/Public/Scripts/PSI/Sale/SREditForm.js
+++ web/Public/Scripts/PSI/Sale/SREditForm.js
@@ -311,10 +311,9 @@ Ext.define("PSI.Sale.SREditForm", {
         if (success) {
           var data = Ext.JSON.decode(response.responseText);
           if (data.success) {
-            PSI.MsgBox.showInfo("成功保存数据", function () {
-              me.close();
-              me.getParentForm().refreshMainGrid(data.id);
-            });
+            me.close();
+            me.getParentForm().refreshMainGrid(data.id);
+            me.tip("成功保存数据");
           } else {
             PSI.MsgBox.showInfo(data.msg);
           }
