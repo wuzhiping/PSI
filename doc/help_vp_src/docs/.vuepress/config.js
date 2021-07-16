9c00125d006e1b737243aaed3f45b1d8f2d5c97c
CRM8000 <crm8000@qq.com>
2021-07-16 10:20:30 +0800

    指南

--- doc/help_vp_src/docs/.vuepress/config.js
+++ doc/help_vp_src/docs/.vuepress/config.js
@@ -1,5 +1,10 @@
 module.exports = {
   lang: 'zh-CN',
   title: 'PSI实施指南',
-  description: 'PSI实施指南'
+  description: 'PSI实施指南',
+
+  themeConfig: {
+    lastUpdated: false,
+    contributors: false,
+  }
 }
