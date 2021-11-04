7352f30021da5856df8fecbfdc57a4a5718c0882
CRM8000 <crm8000@qq.com>
2021-11-04 09:43:26 +0800

    采购退货出库 - 权限控制

--- web/Application/Home/Controller/PurchaseRejController.class.php
+++ web/Application/Home/Controller/PurchaseRejController.class.php
@@ -251,6 +251,11 @@ class PurchaseRejController extends PSIBaseController
   public function deletePRBill()
   {
     if (IS_POST) {
+      $us = new UserService();
+      if (!$us->hasPermission(FIdConst::PURCHASE_REJECTION_DELETE)) {
+        die("没有权限");
+      }
+
       $params = [
         "id" => I("post.id")
       ];
