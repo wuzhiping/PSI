8379da283429c5a09f399639802f7e05d50d1263
CRM8000 <crm8000@qq.com>
2021-11-08 12:10:53 +0800

    查看调拨单 - 界面微调

--- web/Public/Scripts/PSI/Bill/ITViewForm.js
+++ web/Public/Scripts/PSI/Bill/ITViewForm.js
@@ -12,7 +12,10 @@ Ext.define("PSI.Bill.ITViewForm", {
     var me = this;
 
     Ext.apply(me, {
-      title: "查看调拨单",
+      header: {
+        title: "<span style='font-size:160%'>查看调拨单</span>",
+        height: 40
+      },
       modal: true,
       onEsc: Ext.emptyFn,
       maximized: true,
