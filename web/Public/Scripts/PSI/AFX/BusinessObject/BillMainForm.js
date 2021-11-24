14d51e01ab0a4e92ecd7c8e59f6f53286a59a31b
CRM8000 <crm8000@qq.com>
2021-11-24 15:39:37 +0800

    AFX

--- web/Public/Scripts/PSI/AFX/BusinessObject/BillMainForm.js
+++ web/Public/Scripts/PSI/AFX/BusinessObject/BillMainForm.js
@@ -4,6 +4,15 @@
 Ext.define("PSI.AFX.BusinessObject.BillMainForm", {
   extend: "Ext.panel.Panel",
 
+  config: {
+    // 元数据
+    // 主流程如下：
+    // 1、外部先从服务器获得该模块的元数据
+    // 2、然后由外部传入元数据，本class依据元数据创建UI
+    // 3、回调注册的插件，在不修改本class代码的前提下实现外部扩展
+    afxMetadata: null
+  },
+
   mixins: ["PSI.AFX.Mix.Common"],
 
   border: 0,
