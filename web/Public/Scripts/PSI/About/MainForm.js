fa0f21af4f7feb80740e9b13e1327100132c2947
CRM8000 <crm8000@qq.com>
2021-11-27 10:30:37 +0800

    关于 -  调整显示文字

--- web/Public/Scripts/PSI/About/MainForm.js
+++ web/Public/Scripts/PSI/About/MainForm.js
@@ -13,21 +13,18 @@ Ext.define("PSI.About.MainForm", {
 
   modal: true,
   closable: false,
-  width: 520,
+  width: 700,
   layout: "fit",
 
   initComponent: function () {
     var me = this;
 
     var year = new Date().getFullYear();
-    var c = "Copyright &copy; 2015-"
-      + year
-      + " 艾格林门信息服务（大连）有限公司, All Rights Reserved";
+    var c = `Copyright &copy; 2015-${year} 艾格林门信息服务（大连）有限公司, All Rights Reserved`;
 
     Ext.apply(me, {
       header: {
-        title: "<span style='font-size:160%'>关于 - "
-          + me.getProductionName() + "</span>",
+        title: `<span style='font-size:160%'>关于 - ${me.getProductionName()}</span>`,
         iconCls: "PSI-fid-9994",
         height: 40
       },
@@ -38,18 +35,15 @@ Ext.define("PSI.About.MainForm", {
         xtype: "container",
         margin: "0 0 0 10",
         cls: "PSI-about",
-        html: "<h1>欢迎使用"
-          + me.getProductionName()
-          + "</h1><h2>当前版本："
-          + PSI.Const.VERSION
-          + "</h2>"
-          + "<h2>官网  <a style='text-decoration:none;' href='https://gitee.com/crm8000/PSI' target='_blank'>https://gitee.com/crm8000/PSI</a></h2>"
-          + "<p>PHP版本号: "
-          + me.getPhpVersion() + " , MySQL版本号: "
-          + me.getMySQLVersion() + "</p><p>数据库表结构版本号: "
-          + me.getPSIDBVersion() + "</p><p>数据库表结构更新时间: "
-          + me.getPSIDBUpdateDT() + "</p>"
-          + "<p>" + c + "</p>"
+        html: `
+              <h1 style='color:#0050b3'>${me.getProductionName()}基于开源技术，提供人、财、物、产、供、销、存一体化的企业管理全面解决方案</h1>
+              <h3>当前版本：${PSI.Const.VERSION}</h3>
+              <h3>官网&nbsp;<a style='text-decoration:none;' href='https://gitee.com/crm8000/PSI' target='_blank'>https://gitee.com/crm8000/PSI</a></h3>
+              <p>PHP版本号: ${me.getPhpVersion()} , MySQL版本号: ${me.getMySQLVersion()}</p>
+              <p>数据库表结构版本号: ${me.getPSIDBVersion()}</p>
+              <p>数据库表结构更新时间: ${me.getPSIDBUpdateDT()}</p>
+              <p>${c}</p>
+              `
       }],
       buttons: [{
         id: "buttonAboutFormOK",
