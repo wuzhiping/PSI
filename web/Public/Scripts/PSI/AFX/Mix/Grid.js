501310eff6e1f765b4159b436f1785d00079fc0c
CRM8000 <crm8000@qq.com>
2021-11-28 11:25:41 +0800

    AFX

---
+++ web/Public/Scripts/PSI/AFX/Mix/Grid.js
@@ -0,0 +1,10 @@
+/**
+ * 表格相关的Mix
+ * 
+ * @author 李静波
+ */
+Ext.define("PSI.AFX.Mix.Common", {
+  formatGridHeaderTitle: function (title) {
+    return "<span style='font-size:13px'>" + title + "</sapn>";
+  }
+});
