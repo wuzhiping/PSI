23f8349d22ba16144577ac77f8a49a4cd15e65b1
CRM8000 <crm8000@qq.com>
2021-12-05 21:28:05 +0800

    AFX

--- web/Public/Scripts/PSI/AFX/Form/MainForm.js
+++ web/Public/Scripts/PSI/AFX/Form/MainForm.js
@@ -11,4 +11,33 @@ Ext.define("PSI.AFX.Form.MainForm", {
   border: 0,
 
   layout: "border",
+
+  // 保存查询条件里面的input列表，用于在回车的时候跳转
+  __editorList: [],
+  __onEditSpecialKey(field, e) {
+    const me = this;
+
+    if (e.getKey() === e.ENTER) {
+      const id = field.getId();
+      for (let i = 0; i < me.__editorList.length; i++) {
+        const editor = me.__editorList[i];
+        if (id === editor.getId()) {
+          const edit = me.__editorList[i + 1];
+          me.setFocusAndCursorPosToLast(edit);
+        }
+      }
+    }
+  },
+
+  // 最后一个查询input回车后，触发onQuery方法查询数据
+  __onLastEditSpecialKey(field, e) {
+    const me = this;
+
+    if (e.getKey() === e.ENTER) {
+      if (me.onQuery) {
+        me.onQuery.apply(me);
+      }
+    }
+  }
+
 });
