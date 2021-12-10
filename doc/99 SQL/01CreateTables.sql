ce9730e4b25f7659d48cb7200f3816d88f4ce395
CRM8000 <crm8000@qq.com>
2021-12-10 14:32:28 +0800

    系统数据字典

--- doc/99 SQL/01CreateTables.sql
+++ doc/99 SQL/01CreateTables.sql
@@ -1465,6 +1465,28 @@ CREATE TABLE IF NOT EXISTS `t_matrix_bom_config` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+DROP TABLE IF EXISTS `t_dict_table_category`;
+CREATE TABLE IF NOT EXISTS `t_dict_table_category` (
+  `id` varchar(255) NOT NULL,
+  `code` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `parent_id` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `t_dict_table_md`;
+CREATE TABLE IF NOT EXISTS `t_dict_table_md` (
+  `id` varchar(255) NOT NULL,
+  `code` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `table_name` varchar(255) NOT NULL,
+  `category_id` varchar(255) NOT NULL,
+  `memo` varchar(1000) DEFAULT NULL,
+  `py` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
