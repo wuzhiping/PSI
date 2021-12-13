5b1df9327585cf5b0b14f4de6beaa270885c3bbd
CRM8000 <crm8000@qq.com>
2021-12-13 14:54:49 +0800

    代码注释

--- web/Public/Scripts/PSI/Bank/EditForm.js
+++ web/Public/Scripts/PSI/Bank/EditForm.js
@@ -1,7 +1,9 @@
 /**
  * 银行账户 - 新建或编辑界面
  * 
- * @author 李静波
+ * @author 艾格林门信息服务（大连）有限公司
+ * @copyright 2015 - present
+ * @license GPL v3
  */
 Ext.define("PSI.Bank.EditForm", {
   extend: "PSI.AFX.Form.EditForm",
