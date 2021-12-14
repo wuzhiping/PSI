356cfb51f32ddb0a57d4b8617326fd83a9fe735d
CRM8000 <crm8000@qq.com>
2021-12-14 14:50:43 +0800

    代码注释

--- web/Public/Scripts/PSI/InvCheck/InvCheckMainForm.js
+++ web/Public/Scripts/PSI/InvCheck/InvCheckMainForm.js
@@ -1,5 +1,9 @@
 /**
  * 库存盘点 - 主界面
+ * 
+ * @author 艾格林门信息服务（大连）有限公司
+ * @copyright 2015 - present
+ * @license GPL v3
  */
 Ext.define("PSI.InvCheck.InvCheckMainForm", {
   extend: "PSI.AFX.BaseMainExForm",
