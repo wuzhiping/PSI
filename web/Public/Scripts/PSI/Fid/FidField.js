745232831bfa6d5bc03d4b9e498b27e31e995590
CRM8000 <crm8000@qq.com>
2021-12-14 14:35:22 +0800

    代码注释

--- web/Public/Scripts/PSI/Fid/FidField.js
+++ web/Public/Scripts/PSI/Fid/FidField.js
@@ -1,6 +1,10 @@
-//
-// fid自定义字段
-//
+/**
+ * fid自定义字段
+ * 
+ * @author 艾格林门信息服务（大连）有限公司
+ * @copyright 2015 - present
+ * @license GPL v3
+ */
 Ext.define("PSI.Fid.FidField", {
   extend: "Ext.form.field.Trigger",
   alias: "widget.psi_fidfield",
