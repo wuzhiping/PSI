8afa53e911056fe720bdb7f5481962548a6c4ea6
CRM8000 <crm8000@qq.com>
2021-12-14 15:15:53 +0800

    代码注释

--- web/Public/Scripts/PSI/SaleContract/SCMainForm.js
+++ web/Public/Scripts/PSI/SaleContract/SCMainForm.js
@@ -1,6 +1,10 @@
-//
-// 销售合同 - 主界面
-//
+/**
+ * 销售合同 - 主界面
+ * 
+ * @author 艾格林门信息服务（大连）有限公司
+ * @copyright 2015 - present
+ * @license GPL v3
+ */
 Ext.define("PSI.SaleContract.SCMainForm", {
   extend: "PSI.AFX.BaseMainExForm",
 
