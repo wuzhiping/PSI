e2aab30ddb2b59c5da1f0ddce8e170473547920b
CRM8000 <crm8000@qq.com>
2021-12-14 15:17:20 +0800

    代码注释

--- web/Public/Scripts/PSI/SaleOrder/ChangeOrderEditForm.js
+++ web/Public/Scripts/PSI/SaleOrder/ChangeOrderEditForm.js
@@ -1,6 +1,10 @@
-//
-// 销售订单 - 订单变更界面
-//
+/**
+ * 销售订单 - 订单变更界面
+ * 
+ * @author 艾格林门信息服务（大连）有限公司
+ * @copyright 2015 - present
+ * @license GPL v3
+ */
 Ext.define("PSI.SaleOrder.ChangeOrderEditForm", {
   extend: "PSI.AFX.BaseDialogForm",
 
