932b7e4951b96402db482a28b640e369385ec303
CRM8000 <crm8000@qq.com>
2021-12-14 15:10:22 +0800

    代码注释

--- web/Public/Scripts/PSI/PurchaseRej/PRMainForm.js
+++ web/Public/Scripts/PSI/PurchaseRej/PRMainForm.js
@@ -1,5 +1,9 @@
 /**
  * 采购退货出库 - 主界面
+ * 
+ * @author 艾格林门信息服务（大连）有限公司
+ * @copyright 2015 - present
+ * @license GPL v3
  */
 Ext.define("PSI.PurchaseRej.PRMainForm", {
   extend: "PSI.AFX.BaseMainExForm",
