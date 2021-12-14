a86d817f76296f487a5ae1c1048f4a8165afac12
CRM8000 <crm8000@qq.com>
2021-12-14 14:41:12 +0800

    代码注释

--- web/Public/Scripts/PSI/GLPeriod/MainForm.js
+++ web/Public/Scripts/PSI/GLPeriod/MainForm.js
@@ -1,6 +1,10 @@
-//
-// 会计期间 - 主界面
-//
+/**
+ * 会计期间 - 主界面
+ * 
+ * @author 艾格林门信息服务（大连）有限公司
+ * @copyright 2015 - present
+ * @license GPL v3
+ */
 Ext.define("PSI.GLPeriod.MainForm", {
   extend: "PSI.AFX.BaseMainExForm",
 
