a492b672ffc28f14187e50efc4767e56761f28d4
CRM8000 <crm8000@qq.com>
2021-12-18 13:37:01 +0800

    代码注释

--- web/Application/Home/Controller/SaleOrderController.class.php
+++ web/Application/Home/Controller/SaleOrderController.class.php
@@ -10,8 +10,9 @@ use Home\Service\WSBillService;
 /**
  * 销售订单Controller
  *
- * @author 李静波
- *        
+ * @author 艾格林门信息服务（大连）有限公司
+ * @copyright 2015 - present
+ * @license GPL v3
  */
 class SaleOrderController extends PSIBaseController
 {
