2b13f4803281ba980ed82f04d3abe651e9ae3208
CRM8000 <crm8000@qq.com>
2022-01-03 08:57:02 +0800

    指南 - init

--- doc/help_src_vuepress/docs/.vuepress/config.js
+++ doc/help_src_vuepress/docs/.vuepress/config.js
@@ -8,5 +8,24 @@ module.exports = {
   base: '/help/',
 
   themeConfig: {
+    editLink: false,
+    contributors: false,
+    backToHome: '返回首页',
+    notFound: ['文档页面不存在'],
+    lastUpdatedText: '文档更新时间',
+    navbar: [
+      {
+        text: '使用手册',
+        link: '/user/'
+      },
+      {
+        text: '系统管理',
+        link: '/admin/'
+      },
+      {
+        text: '二次开发',
+        link: '/dev/'
+      }
+    ]
   },
 }
