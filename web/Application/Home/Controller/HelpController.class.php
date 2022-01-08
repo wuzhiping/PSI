0d7a79bbed5ec37749bcf4866cf254028c389e8c
CRM8000 <crm8000@qq.com>
2022-01-08 10:34:29 +0800

    实施指南

--- web/Application/Home/Controller/HelpController.class.php
+++ web/Application/Home/Controller/HelpController.class.php
@@ -5,7 +5,7 @@ namespace Home\Controller;
 use Home\Service\BizlogService;
 
 /**
- * 帮助Controller
+ * 实施指南Controller
  *
  * @author 艾格林门信息服务（大连）有限公司
  * @copyright 2015 - present
@@ -21,143 +21,143 @@ class HelpController extends PSIBaseController
     $key = I("get.t");
     switch ($key) {
       case "login":
-        // 用户直接访问登录帮助的时候，多半还没有登录所以没法记录业务日志
+        // 用户直接访问登录实施指南的时候，多半还没有登录所以没法记录业务日志
         redirect("/help/user/10.html");
         break;
       case "user":
-        $bs->insertBizlog("访问帮助页面：用户管理", "帮助");
+        $bs->insertBizlog("访问实施指南页面：用户管理", "实施指南");
         redirect("/help/admin/02-01.html");
         break;
       case "priceSystem":
-        $bs->insertBizlog("访问帮助页面：价格体系", "帮助");
+        $bs->insertBizlog("访问实施指南页面：价格体系", "实施指南");
         redirect("/help/admin/02-04-03.html");
         break;
       case "initInv":
-        $bs->insertBizlog("访问帮助页面：库存建账", "帮助");
+        $bs->insertBizlog("访问实施指南页面：库存建账", "实施指南");
         redirect("/help/admin/02-06.html");
         break;
       case "permission":
-        $bs->insertBizlog("访问帮助页面：权限管理", "帮助");
+        $bs->insertBizlog("访问实施指南页面：权限管理", "实施指南");
         redirect("/help/admin/02-02.html");
         break;
       case "bizlog":
-        $bs->insertBizlog("访问帮助页面：业务日志", "帮助");
+        $bs->insertBizlog("访问实施指南页面：业务日志", "实施指南");
         redirect("/help/admin/03.html");
         break;
       case "warehouse":
-        $bs->insertBizlog("访问帮助页面：仓库", "帮助");
+        $bs->insertBizlog("访问实施指南页面：仓库", "实施指南");
         redirect("/help/admin/02-05.html");
         break;
       case "goods":
-        $bs->insertBizlog("访问帮助页面：物料", "帮助");
+        $bs->insertBizlog("访问实施指南页面：物料", "实施指南");
         redirect("/help/admin/02-04.html");
         break;
       case "goodsBrand":
-        $bs->insertBizlog("访问帮助页面：物料品牌", "帮助");
+        $bs->insertBizlog("访问实施指南页面：物料品牌", "实施指南");
         redirect("/help/admin/02-04-02.html");
         break;
       case "goodsUnit":
-        $bs->insertBizlog("访问帮助页面：物料计量单位", "帮助");
+        $bs->insertBizlog("访问实施指南页面：物料计量单位", "实施指南");
         redirect("/help/admin/02-04-01.html");
         break;
       case "supplier":
-        $bs->insertBizlog("访问帮助页面：供应商档案", "帮助");
+        $bs->insertBizlog("访问实施指南页面：供应商档案", "实施指南");
         redirect("/help/admin/02-07.html");
         break;
       case "customer":
-        $bs->insertBizlog("访问帮助页面：客户资料", "帮助");
+        $bs->insertBizlog("访问实施指南页面：客户资料", "实施指南");
         redirect("/help/admin/02-08.html");
         break;
       case "bizconfig":
-        $bs->insertBizlog("访问帮助页面：业务设置", "帮助");
+        $bs->insertBizlog("访问实施指南页面：业务设置", "实施指南");
         redirect("/help/admin/02-03.html");
         break;
       case "pobill":
-        $bs->insertBizlog("访问帮助页面：采购订单", "帮助");
+        $bs->insertBizlog("访问实施指南页面：采购订单", "实施指南");
         redirect("/help/user/20-01.html");
         break;
       case "pwbill":
-        $bs->insertBizlog("访问帮助页面：采购入库", "帮助");
+        $bs->insertBizlog("访问实施指南页面：采购入库", "实施指南");
         redirect("/help/user/20-02.html");
         break;
       case "prbill":
-        $bs->insertBizlog("访问帮助页面：采购退货出库", "帮助");
+        $bs->insertBizlog("访问实施指南页面：采购退货出库", "实施指南");
         redirect("/help/user/20-03.html");
         break;
       case "sobill":
-        $bs->insertBizlog("访问帮助页面：销售订单", "帮助");
+        $bs->insertBizlog("访问实施指南页面：销售订单", "实施指南");
         redirect("/help/user/30-01.html");
         break;
       case "wsbill":
-        $bs->insertBizlog("访问帮助页面：销售出库", "帮助");
+        $bs->insertBizlog("访问实施指南页面：销售出库", "实施指南");
         redirect("/help/user/30-02.html");
         break;
       case "srbill":
-        $bs->insertBizlog("访问帮助页面：销售退货入库", "帮助");
+        $bs->insertBizlog("访问实施指南页面：销售退货入库", "实施指南");
         redirect("/help/user/30-03.html");
         break;
       case "itbill":
-        $bs->insertBizlog("访问帮助页面：库间调拨", "帮助");
+        $bs->insertBizlog("访问实施指南页面：库间调拨", "实施指南");
         redirect("/help/user/40-01.html");
         break;
       case "icbill":
-        $bs->insertBizlog("访问帮助页面：库存盘点", "帮助");
+        $bs->insertBizlog("访问实施指南页面：库存盘点", "实施指南");
         redirect("/help/user/40-02.html");
         break;
       case "dataOrg":
-        $bs->insertBizlog("访问帮助页面：数据域应用详解", "帮助");
+        $bs->insertBizlog("访问实施指南页面：数据域应用详解", "实施指南");
         redirect("/help/admin/05.html");
         break;
       case "commBill":
-        $bs->insertBizlog("访问帮助页面：表单通用操作", "帮助");
+        $bs->insertBizlog("访问实施指南页面：表单通用操作", "实施指南");
         redirect("/help/user/00.html");
         break;
       case "scbill":
-        $bs->insertBizlog("访问帮助页面：销售合同", "帮助");
+        $bs->insertBizlog("访问实施指南页面：销售合同", "实施指南");
         redirect("/help/user/30-04.html");
         break;
       case "costWeight":
-        $bs->insertBizlog("访问帮助页面：BOM-成本分摊权重", "帮助");
+        $bs->insertBizlog("访问实施指南页面：BOM-成本分摊权重", "实施指南");
         redirect("/help/admin/02-04-04.html");
         break;
       case "wspbill":
-        $bs->insertBizlog("访问帮助页面：存货拆分", "帮助");
+        $bs->insertBizlog("访问实施指南页面：存货拆分", "实施指南");
         redirect("/help/user/60-01.html");
         break;
       case "factory":
-        $bs->insertBizlog("访问帮助页面：工厂", "帮助");
+        $bs->insertBizlog("访问实施指南页面：工厂", "实施指南");
         redirect("/help/admin/02-09.html");
         break;
       case "dmobill":
-        $bs->insertBizlog("访问帮助页面：成品委托生产订单", "帮助");
+        $bs->insertBizlog("访问实施指南页面：成品委托生产订单", "实施指南");
         redirect("/help/user/60-02.html");
         break;
       case "dmwbill":
-        $bs->insertBizlog("访问帮助页面：成品委托生产入库", "帮助");
+        $bs->insertBizlog("访问实施指南页面：成品委托生产入库", "实施指南");
         redirect("/help/user/60-03.html");
         break;
         // case "mainMenuMaintain":
-        //   $bs->insertBizlog("访问帮助页面：主菜单维护", "帮助");
+        //   $bs->insertBizlog("访问实施指南页面：主菜单维护", "实施指南");
         //   redirect("/help/08-01.html");
         //   break;
       case "sysdict":
-        $bs->insertBizlog("访问帮助页面：系统数据字典", "帮助");
+        $bs->insertBizlog("访问实施指南页面：系统数据字典", "实施指南");
         redirect("/help/dev/lcap/08-02.html");
         break;
         // case "codetable":
-        //   $bs->insertBizlog("访问帮助页面：码表设置", "帮助");
+        //   $bs->insertBizlog("访问实施指南页面：码表设置", "实施指南");
         //   redirect("/help/08-03.html");
         //   break;
         // case "formview":
-        //   $bs->insertBizlog("访问帮助页面：视图开发助手", "帮助");
+        //   $bs->insertBizlog("访问实施指南页面：视图开发助手", "实施指南");
         //   redirect("/help/08-04.html");
         //   break;
       case "solution":
-        $bs->insertBizlog("访问指南页面：解决方案", "实施指南");
+        $bs->insertBizlog("访问实施指南页面：解决方案", "实施指南");
         redirect("/help/dev/lcap/08-05.html");
         break;
       default:
-        $bs->insertBizlog("通过主菜单进入帮助页面", "帮助");
+        $bs->insertBizlog("通过主菜单进入实施指南页面", "实施指南");
         redirect("/help/index.html");
     }
   }
