001f9eff5499b9dd252cc3fab74bd4d117deea3d
CRM8000 <crm8000@qq.com>
2022-01-10 14:31:55 +0800

    数据库文档 - PSI标准解决方案

---
+++ doc/99 SQL/04InitSolution.sql
@@ -0,0 +1,10 @@
+# @author 艾格林门信息服务（大连）有限公司
+# @copyright 2015 - present
+# @license GPL v3
+
+TRUNCATE TABLE `t_solution`;
+
+insert into t_solution (id, code, name) values
+('D5B0A2DE-71DE-11EC-97BD-E86A641ED142', 'SLN0000', 'PSI低代码应用平台'),
+('E0D4BBE8-71DE-11EC-97BD-E86A641ED142', 'SLN0001', 'PSI标准进销存'),
+('ECA855E8-71DE-11EC-97BD-E86A641ED142', 'SLN0002', 'PSI财务总账'); 
