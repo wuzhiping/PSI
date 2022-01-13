c27410bb1bdd7d7f12007d40da2e181d41b99b99
CRM8000 <crm8000@qq.com>
2022-01-13 14:49:47 +0800

    码表 - 演示环境下，禁止操作

--- web/Application/Home/Service/CodeTableService.class.php
+++ web/Application/Home/Service/CodeTableService.class.php
@@ -38,6 +38,11 @@ class CodeTableService extends PSIBaseExService
     }
 
     $id = $params["id"];
+    if ($this->isDemo()) {
+      $n = $id ? "编辑" : "新建";
+      return $this->bad("演示环境下，不能{$n}码表分类");
+    }
+
     $name = $params["name"];
 
     $db = $this->db();
@@ -82,6 +87,10 @@ class CodeTableService extends PSIBaseExService
   {
     if ($this->isNotOnline()) {
       return $this->notOnlineError();
+    }
+
+    if ($this->isDemo()) {
+      return $this->bad("演示环境下，不能删除码表分类");
     }
 
     $db = $this->db();
