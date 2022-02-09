ff9b9bf14ec1055448b69494926a83f3467272b3
CRM8000 <crm8000@qq.com>
2022-02-09 13:42:26 +0800

    数据库文档

--- doc/99 SQL/03InitSysDict.sql
+++ doc/99 SQL/03InitSysDict.sql
@@ -66,7 +66,10 @@ INSERT INTO `t_sysdict_mainmenu_nav` (`id`, `code`, `code_int`, `name`, `py`, `m
 ('1003', '1003', 1003, '/Home/Warehouse', '', '仓库', 8),
 ('1004', '1004', 1004, '/Home/Supplier', '', '供应商档案', 9),
 ('1007', '1007', 1007, '/Home/Customer', '', '客户资料', 10),
-('2000', '2000', 2000, '/Home/Inventory/initIndex', '', '库存建账', 11)
+('2000', '2000', 2000, '/Home/Inventory/initIndex', '', '库存建账', 11),
+('2001', '2001', 2001, '/Home/Purchase/pwbillIndex', '', '采购入库', 12),
+('2003', '2003', 2003, '/Home/Inventory/inventoryQuery', '', '库存账查询', 13),
+('2005', '2005', 2005, '/Home/Funds/payIndex', '', '应付账款管理', 14)
 ;
 
 
