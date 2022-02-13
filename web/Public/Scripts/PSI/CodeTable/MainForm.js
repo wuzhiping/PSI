1438d89ee392e39706566f37bb65166b146bbf80
CRM8000 <crm8000@qq.com>
2022-02-13 10:20:28 +0800

    码表

--- web/Public/Scripts/PSI/CodeTable/MainForm.js
+++ web/Public/Scripts/PSI/CodeTable/MainForm.js
@@ -755,6 +755,14 @@ Ext.define("PSI.CodeTable.MainForm", {
   _onEditCodeTable() {
     const me = this;
 
+    const slnCode = me.comboSolution.getValue();
+    const sln = me.comboSolution.findRecordByValue(slnCode);
+    if (!sln) {
+      me.showInfo("没有选择解决方案");
+      return;
+    }
+    const slnName = sln.get("name");
+
     const item = me.getMainGrid().getSelectionModel().getSelection();
     if (item == null || item.length != 1) {
       me.showInfo("请选择要编辑的码表");
@@ -765,7 +773,9 @@ Ext.define("PSI.CodeTable.MainForm", {
 
     const form = Ext.create("PSI.CodeTable.CodeTableEditForm", {
       parentForm: me,
-      entity: codeTable
+      entity: codeTable,
+      slnCode,
+      slnName,
     });
     form.show();
   },
