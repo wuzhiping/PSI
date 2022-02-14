770c894c2cb5e11365dd064c6d2cd9c8b2a40794
CRM8000 <crm8000@qq.com>
2022-02-14 09:56:56 +0800

    码表

---
+++ web/Public/Scripts/PSI/CodeTable/CodeTableSolutionGenSQLForm.js
@@ -0,0 +1,93 @@
+/**
+ * 导出SQL
+ * 
+ * @author 艾格林门信息服务（大连）有限公司
+ * @copyright 2015 - present
+ * @license GPL v3
+ */
+Ext.define("PSI.CodeTable.CodeTableSolutionGenSQLForm", {
+  extend: "Ext.window.Window",
+
+  mixins: ["PSI.AFX.Mix.Common"],
+
+  config: {
+    codeTable: null
+  },
+
+  initComponent() {
+    const me = this;
+
+    Ext.apply(me, {
+      title: "导出SQL",
+      width: 800,
+      height: 420,
+      layout: "fit",
+      items: [{
+        id: "editForm",
+        xtype: "form",
+        layout: "form",
+        height: "100%",
+        bodyPadding: 5,
+        defaultType: 'textfield',
+        fieldDefaults: {
+          labelWidth: 60,
+          labelAlign: "right",
+          labelSeparator: "",
+          msgTarget: 'side'
+        },
+        items: [{
+          id: "editSQL",
+          fieldLabel: "SQL",
+          xtype: "textareafield",
+          height: 300,
+          readOnly: true
+        }],
+        buttons: [{
+          text: "关闭",
+          handler() {
+            me.close();
+          },
+          scope: me
+        }]
+      }],
+      listeners: {
+        show: {
+          fn: me.onWndShow,
+          scope: me
+        }
+      }
+    });
+
+    me.callParent(arguments);
+  },
+
+  onWndShow() {
+    const me = this;
+
+    const el = me.getEl();
+    el && el.mask(PSI.Const.LOADING);
+    const r = {
+      url: me.URL("Home/CodeTable/codeTableSolutionGenSQL"),
+      params: {
+        id: me.getCodeTable().get("id")
+      },
+      callback(options, success, response) {
+        if (success) {
+          const data = me.decodeJSON(response.responseText);
+
+          if (data.success) {
+            Ext.getCmp("editSQL").setValue(data.sql);
+          } else {
+            Ext.getCmp("editSQL").setValue(data.msg);
+          }
+        } else {
+          me.showInfo("网络错误");
+        }
+
+        el && el.unmask();
+      }
+    };
+
+    me.ajax(r);
+  }
+});
