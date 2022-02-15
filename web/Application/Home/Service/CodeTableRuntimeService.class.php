07067204fa2c3e810dfbe8d7169d0d490b8a1032
CRM8000 <crm8000@qq.com>
2022-02-15 13:07:53 +0800

    码表

--- web/Application/Home/Service/CodeTableRuntimeService.class.php
+++ web/Application/Home/Service/CodeTableRuntimeService.class.php
@@ -128,64 +128,6 @@ class CodeTableRuntimeService extends PSIBaseExService
   }
 
   /**
-   * 新增或编辑码表列
-   */
-  public function editCodeTableCol($params)
-  {
-    if ($this->isNotOnline()) {
-      return $this->notOnlineError();
-    }
-
-    $id = $params["id"];
-    if ($this->isDemo()) {
-      $n = $id ? "编辑" : "新建";
-      return $this->bad("演示环境下，不能{$n}码表列");
-    }
-
-    $isModify = $id;
-    $name = $params["name"];
-
-    $pyService = new PinyinService();
-    $py = $pyService->toPY($name);
-    $params["py"] = $py;
-
-    $db = $this->db();
-    $db->startTrans();
-
-    $log = null;
-    $dao = new CodeTableRuntimeDAO($db);
-    if ($isModify) {
-      // 编辑
-      $rc = $dao->updateCodeTableCol($params);
-      if ($rc) {
-        $db->rollback();
-        return $rc;
-      }
-    } else {
-      // 新增
-      $rc = $dao->addCodeTableCol($params);
-      if ($rc) {
-        $db->rollback();
-        return $rc;
-      }
-
-      $id = $params["id"];
-    }
-
-    // 记录业务日志
-    $log = $params["log"];
-    $bs = new BizlogService($db);
-    $bs->insertBizlog($log, $this->LOG_CATEGORY);
-
-    if ($isModify) {
-      // 新增字段的时候，执行了DDL语句会自动提交事务
-      $db->commit();
-    }
-
-    return $this->ok($id);
-  }
-
-  /**
    * 查询码表记录的详情
    */
   public function recordInfo($params)
@@ -200,7 +142,6 @@ class CodeTableRuntimeService extends PSIBaseExService
     $dao = new CodeTableRuntimeDAO($this->db());
     return $dao->recordInfo($params);
   }
-
 
   /**
    * 删除码表记录
@@ -237,53 +178,6 @@ class CodeTableRuntimeService extends PSIBaseExService
   }
 
   /**
-   * 码表某列的详细信息
-   */
-  public function codeTableColInfo($params)
-  {
-    if ($this->isNotOnline()) {
-      return $this->emptyResult();
-    }
-
-    $dao = new CodeTableRuntimeDAO($this->db());
-    return $dao->codeTableColInfo($params);
-  }
-
-  /**
-   * 删除码表列
-   */
-  public function deleteCodeTableCol($params)
-  {
-    if ($this->isNotOnline()) {
-      return $this->notOnlineError();
-    }
-
-    if ($this->isDemo()) {
-      return $this->bad("演示环境下，不能删除码表列");
-    }
-
-    $db = $this->db();
-    $db->startTrans();
-
-    $dao = new CodeTableRuntimeDAO($db);
-    $rc = $dao->deleteCodeTableCol($params);
-    if ($rc) {
-      $db->rollback();
-      return $rc;
-    }
-
-    $log = $params["log"];
-
-    // 记录业务日志
-    $bs = new BizlogService($db);
-    $bs->insertBizlog($log, $this->LOG_CATEGORY);
-
-    $db->commit();
-
-    return $this->ok();
-  }
-
-  /**
    * 码表记录引用字段 - 查询数据
    */
   public function queryDataForRecordRef($params)
@@ -296,42 +190,6 @@ class CodeTableRuntimeService extends PSIBaseExService
 
     $dao = new CodeTableRuntimeDAO($this->db());
     return $dao->queryDataForRecordRef($params);
-  }
-
-  /**
-   * 把码表转化为系统固有码表
-   */
-  public function convertCodeTable($params)
-  {
-    if ($this->isNotOnline()) {
-      return $this->notOnlineError();
-    }
-
-    if ($this->isDemo()) {
-      return $this->bad("在演示环境下，不能使用本功能，请见谅");
-    }
-
-
-    $db = $this->db();
-    $db->startTrans();
-
-    $dao = new CodeTableRuntimeDAO($db);
-    $rc = $dao->convertCodeTable($params);
-    if ($rc) {
-      $db->rollback();
-      return $rc;
-    }
-
-    $name = $params["name"];
-    $log = "把码表[{$name}]转化为系统固有码表";
-
-    // 记录业务日志
-    $bs = new BizlogService($db);
-    $bs->insertBizlog($log, $this->LOG_CATEGORY);
-
-    $db->commit();
-
-    return $this->ok();
   }
 
   /**
@@ -363,88 +221,5 @@ class CodeTableRuntimeService extends PSIBaseExService
     $db->commit();
 
     return $this->ok();
-  }
-
-  /**
-   * 查询码表编辑界面字段的显示次序
-   */
-  public function queryCodeTableEditColShowOrder($params)
-  {
-    if ($this->isNotOnline()) {
-      return $this->emptyResult();
-    }
-
-    $dao = new CodeTableRuntimeDAO($this->db());
-    return $dao->queryCodeTableEditColShowOrder($params);
-  }
-
-  /**
-   * 保存编辑界面字段显示次序
-   */
-  public function saveColEditShowOrder($params)
-  {
-    if ($this->isNotOnline()) {
-      return $this->notOnlineError();
-    }
-
-    $db = $this->db();
-    $db->startTrans();
-
-    $dao = new CodeTableRuntimeDAO($db);
-    $rc = $dao->saveColEditShowOrder($params);
-    if ($rc) {
-      $db->rollback();
-      return $rc;
-    }
-
-    $name = $params["name"];
-    $log = "保存码表[{$name}]编辑界面字段显示次序";
-
-    // 记录业务日志
-    $bs = new BizlogService($db);
-    $bs->insertBizlog($log, $this->LOG_CATEGORY);
-
-    $db->commit();
-
-    return $this->ok();
-  }
-
-  /**
-   * 码表生成SQL
-   */
-  public function codeTableGenSQL($params)
-  {
-    if ($this->isNotOnline()) {
-      return $this->emptyResult();
-    }
-
-    $dao = new CodeTableRuntimeDAO($this->db());
-    return $dao->codeTableGenSQL($params);
-  }
-
-  /**
-   * 解决方案的全部码表生成SQL
-   */
-  public function codeTableSolutionGenSQL($params)
-  {
-    if ($this->isNotOnline()) {
-      return $this->emptyResult();
-    }
-
-    $dao = new CodeTableRuntimeDAO($this->db());
-    return $dao->codeTableSolutionGenSQL($params);
-  }
-
-  /**
-   * 查询解决方案列表
-   */
-  public function querySolutionList()
-  {
-    if ($this->isNotOnline()) {
-      return $this->emptyResult();
-    }
-
-    $dao = new CodeTableRuntimeDAO($this->db());
-    return $dao->querySolutionList();
   }
 }
