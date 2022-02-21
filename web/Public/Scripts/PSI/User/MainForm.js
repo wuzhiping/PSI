116d3264a3a12a2e37af3aeeeab34545195dff44
CRM8000 <crm8000@qq.com>
2022-02-21 14:06:15 +0800

    用户管理 - 应用PCL

--- web/Public/Scripts/PSI/User/MainForm.js
+++ web/Public/Scripts/PSI/User/MainForm.js
@@ -6,37 +6,9 @@
  * @license GPL v3
  */
 
-///==================================
-/// 在这个文件里面会编写大量的ExtJS编程技巧的注释
-/// 以帮助大家掌握其常见套路
-///==================================
-
-/// 知识点1：Ext.define
-/// 这是在ExtJS中定义class的方法
-/// 在ExtJS创立的年代，JS中并没有class，为了实现OOP，ExtJS采用这种自己独创的方式
-/// Ext.define(类名, JSON对象);
-/// 每个JS中都是上述的套路
-
-Ext.define("PSI.User.MainForm", {
-
-  /// 知识点2：继承
-  /// 通过extend属性，实现继承
-  /// 本例中PSI.User.MainForm 继承了PSI.AFX.Form.MainForm
+PCL.define("PSI.User.MainForm", {
   extend: "PSI.AFX.Form.MainForm",
 
-  /// 知识点3：配置项，以及通过配置项在claas创建的时候，传入参数
-  /// 配置项会生成对应的方法
-  /// 例如下面的pAddOrg会生成 getPAddOrg()方法，该方法可以获得pAddOrg的值。
-  /// 那么它的值是怎么传入的呢？
-  /// \web\Application\Home\View\User\index.html 中
-  /// 用Ext.create创建本class的时候，第二个参数中就传入了pAddOrg等参数
-  /// 因为\web\Application\Home\View\User\index.html对应后台的Controller的一个方法
-  /// 这样整个流程就实现了从后台取数，然后传递给ExtJS class
-  /// 
-  /// 用配置项，也能实现在ExtJS class之间传递参数
-  /// 在PSI中最常用的模式是：在MainForm中创建其他的Form的时候，把MainForm自身作为parentForm传递给
-  /// 其他Form，以便其他Form回调MainForm中的方法
-  /// 具体例子参见：本class的_onAddOrg方法  
   config: {
     pAddOrg: null,
     pEditOrg: null,
@@ -47,34 +19,20 @@ Ext.define("PSI.User.MainForm", {
     pChangePassword: null
   },
 
-  /// 知识点4：initComponent
-  /// 创建UI的关键方法
-  /// ExtJS用了模板方法设计模式(Template Method)
-  /// 该设计模式也形象地称为【三明治模式】，具体见方法内的注释
   /**
    * 初始化组件
    * 
    * @override
    */
   initComponent() {
-    /// 把this用me局部变量保存，然后后续的代码中都使用me，而不使用this
-    /// 这是避免JS this是上下文绑定这个特性带了的惊喜
-    /// 用me这个名称，是ExtJS的惯例
     const me = this;
 
-    /// 三明治模式的第一层
-    /// 通常就是 Ext.apply(...)
-    Ext.apply(me, {
+    PCL.apply(me, {
       items: [{
         tbar: [{
           text: "新建组织机构",
           disabled: me.getPAddOrg() == "0",
           handler: me._onAddOrg,
-          /// 特别知识点：scope
-          /// 在给组件绑定事件的时候，
-          /// 通常把scope也赋值为me
-          /// 这是进入避免JS this上下文绑定带来的莫名惊喜
-          /// 这样确保me._onAddOrg中的this是me
           scope: me
         }, {
           text: "编辑组织机构",
@@ -150,14 +108,12 @@ Ext.define("PSI.User.MainForm", {
       }]
     });
 
-    /// 三明治模式第二层：调用父类方法
     me.callParent(arguments);
 
-    /// 三明治模式第三层：使用创建的组件
     me.orgTree = me.getOrgGrid();
 
     // 查询控件input List
-    me.__editorList = [Ext.getCmp("editQueryLoginName"), Ext.getCmp("editQueryName"), Ext.getCmp("editQueryEnabled")];
+    me.__editorList = [PCL.getCmp("editQueryLoginName"), PCL.getCmp("editQueryName"), PCL.getCmp("editQueryEnabled")];
   },
 
   /**
@@ -204,7 +160,7 @@ Ext.define("PSI.User.MainForm", {
       labelSeparator: "",
       fieldLabel: "状态",
       margin: "5, 0, 0, 0",
-      store: Ext.create("Ext.data.ArrayStore", {
+      store: PCL.create("Ext.data.ArrayStore", {
         fields: ["id", "text"],
         data: [[-1, "全部"], [1, "允许登录"], [0, "禁止登录"]]
       }),
@@ -241,7 +197,7 @@ Ext.define("PSI.User.MainForm", {
         iconCls: "PSI-button-hide",
         margin: "5 0 0 10",
         handler: function () {
-          Ext.getCmp("panelQueryCmp").collapse();
+          PCL.getCmp("panelQueryCmp").collapse();
         },
         scope: me
       }]
@@ -258,13 +214,13 @@ Ext.define("PSI.User.MainForm", {
     }
 
     const modelName = "PSIModel.PSI.User.OrgModel";
-    Ext.define(modelName, {
-      extend: "Ext.data.Model",
+    PCL.define(modelName, {
+      extend: "PCL.data.Model",
       fields: ["id", "text", "fullName", "orgCode", "dataOrg",
         "leaf", "children", "userCount", "orgType"]
     });
 
-    const orgStore = Ext.create("Ext.data.TreeStore", {
+    const orgStore = PCL.create("PCL.data.TreeStore", {
       model: modelName,
       proxy: {
         type: "ajax",
@@ -285,7 +241,7 @@ Ext.define("PSI.User.MainForm", {
 
     orgStore.on("load", me._onOrgStoreLoad, me);
 
-    const orgTree = Ext.create("Ext.tree.Panel", {
+    const orgTree = PCL.create("PCL.tree.Panel", {
       cls: "PSI",
       header: {
         height: 30,
@@ -300,7 +256,7 @@ Ext.define("PSI.User.MainForm", {
       tools: [{
         type: "close",
         handler() {
-          Ext.getCmp("panelOrg").collapse();
+          PCL.getCmp("panelOrg").collapse();
         }
       }],
       columns: {
@@ -357,13 +313,13 @@ Ext.define("PSI.User.MainForm", {
     }
 
     const modelName = "PSIModel.PSI.User.UserModel";
-    Ext.define(modelName, {
-      extend: "Ext.data.Model",
+    PCL.define(modelName, {
+      extend: "PCL.data.Model",
       fields: ["id", "loginName", "name", "enabled", "orgCode",
         "gender", "birthday", "idCardNumber", "tel",
         "tel02", "address", "dataOrg", "roleName"]
     });
-    const storeGrid = Ext.create("Ext.data.Store", {
+    const storeGrid = PCL.create("PCL.data.Store", {
       autoLoad: false,
       model: modelName,
       data: [],
@@ -384,7 +340,7 @@ Ext.define("PSI.User.MainForm", {
       storeGrid.proxy.extraParams = me.getUserParam();
     });
 
-    me._userGrid = Ext.create("Ext.grid.Panel", {
+    me._userGrid = PCL.create("PCL.grid.Panel", {
       cls: "PSI",
       header: {
         height: 30,
@@ -399,7 +355,7 @@ Ext.define("PSI.User.MainForm", {
           menuDisabled: true,
           sortable: false,
         },
-        items: [Ext.create("Ext.grid.RowNumberer", {
+        items: [PCL.create("PCL.grid.RowNumberer", {
           text: "#",
           width: 40
         }), {
@@ -489,7 +445,7 @@ Ext.define("PSI.User.MainForm", {
           xtype: "combobox",
           editable: false,
           width: 60,
-          store: Ext.create("Ext.data.ArrayStore", {
+          store: PCL.create("Ext.data.ArrayStore", {
             fields: ["text"],
             data: [["20"], ["50"], ["100"], ["300"],
             ["1000"]]
@@ -498,9 +454,9 @@ Ext.define("PSI.User.MainForm", {
           listeners: {
             change: {
               fn() {
-                storeGrid.pageSize = Ext.getCmp("comboCountPerPage").getValue();
+                storeGrid.pageSize = PCL.getCmp("comboCountPerPage").getValue();
                 storeGrid.currentPage = 1;
-                Ext.getCmp("pagingToolbar").doRefresh();
+                PCL.getCmp("pagingToolbar").doRefresh();
               },
               scope: me
             }
@@ -522,11 +478,7 @@ Ext.define("PSI.User.MainForm", {
   _onAddOrg() {
     const me = this;
 
-    /// 知识点5：用Ext.create创建ExtJS class，不要是new创建ExtJS class
-    /// Ext.create(类名, 配置项JSON对象)
-    const form = Ext.create("PSI.User.OrgEditForm", {
-      /// 这就是通过配置项在ExtJS组件之间传递参数
-      /// 参考PSI.User.OrgEditForm的onOK方法中如何回调本class的方法的
+    const form = PCL.create("PSI.User.OrgEditForm", {
       parentForm: me
     });
     form.show();
@@ -552,7 +504,7 @@ Ext.define("PSI.User.MainForm", {
 
     const org = item[0];
 
-    const form = Ext.create("PSI.User.OrgEditForm", {
+    const form = PCL.create("PSI.User.OrgEditForm", {
       parentForm: me,
       entity: org
     });
@@ -576,14 +528,14 @@ Ext.define("PSI.User.MainForm", {
     const org = item[0].getData();
 
     const funcConfirm = () => {
-      Ext.getBody().mask("正在删除中...");
+      PCL.getBody().mask("正在删除中...");
       const r = {
         url: me.URL("Home/User/deleteOrg"),
         params: {
           id: org.id
         },
         callback(options, success, response) {
-          Ext.getBody().unmask();
+          PCL.getBody().unmask();
 
           if (success) {
             const data = me.decodeJSON(response.responseText);
@@ -643,7 +595,7 @@ Ext.define("PSI.User.MainForm", {
       org = item[0];
     }
 
-    const form = Ext.create("PSI.User.UserEditForm", {
+    const form = PCL.create("PSI.User.UserEditForm", {
       parentForm: me,
       defaultOrg: org
     });
@@ -669,7 +621,7 @@ Ext.define("PSI.User.MainForm", {
 
     const user = item[0];
 
-    const form = Ext.create("PSI.User.UserEditForm", {
+    const form = PCL.create("PSI.User.UserEditForm", {
       parentForm: me,
       entity: user
     });
@@ -691,7 +643,7 @@ Ext.define("PSI.User.MainForm", {
     }
 
     const user = item[0].getData();
-    const form = Ext.create("PSI.User.ChangeUserPasswordForm", {
+    const form = PCL.create("PSI.User.ChangeUserPasswordForm", {
       entity: user
     });
     form.show();
@@ -713,14 +665,14 @@ Ext.define("PSI.User.MainForm", {
     const user = item[0].getData();
 
     const funcConfirm = () => {
-      Ext.getBody().mask("正在删除中...");
+      PCL.getBody().mask("正在删除中...");
       const r = {
         url: me.URL("Home/User/deleteUser"),
         params: {
           id: user.id
         },
         callback(options, success, response) {
-          Ext.getBody().unmask();
+          PCL.getBody().unmask();
 
           if (success) {
             const data = me.decodeJSON(response.responseText);
@@ -758,7 +710,7 @@ Ext.define("PSI.User.MainForm", {
 
     grid.setTitle(me.formatGridHeaderTitle(`<span class='PSI-title-keyword'>${org.fullName}</span> - 人员列表`));
 
-    Ext.getCmp("pagingToolbar").doRefresh();
+    PCL.getCmp("pagingToolbar").doRefresh();
   },
 
   /**
@@ -790,19 +742,19 @@ Ext.define("PSI.User.MainForm", {
     const org = item[0];
 
     let queryLoginName = null;
-    const editLoginName = Ext.getCmp("editQueryLoginName");
+    const editLoginName = PCL.getCmp("editQueryLoginName");
     if (editLoginName) {
       queryLoginName = editLoginName.getValue();
     }
 
     let queryName = null;
-    const editQueryName = Ext.getCmp("editQueryName");
+    const editQueryName = PCL.getCmp("editQueryName");
     if (editQueryName) {
       queryName = editQueryName.getValue();
     }
 
     let enabled = -1;
-    const edit = Ext.getCmp("editQueryEnabled");
+    const edit = PCL.getCmp("editQueryEnabled");
     if (edit) {
       enabled = edit.getValue();
     }
@@ -823,9 +775,9 @@ Ext.define("PSI.User.MainForm", {
   _onClearQuery() {
     const me = this;
 
-    Ext.getCmp("editQueryLoginName").setValue(null);
-    Ext.getCmp("editQueryName").setValue(null);
-    Ext.getCmp("editQueryEnabled").setValue(-1);
+    PCL.getCmp("editQueryLoginName").setValue(null);
+    PCL.getCmp("editQueryName").setValue(null);
+    PCL.getCmp("editQueryEnabled").setValue(-1);
 
     me._onQuery();
   },
@@ -846,19 +798,19 @@ Ext.define("PSI.User.MainForm", {
    */
   getQueryParamForCategory() {
     let queryLoginName = null;
-    const editLoginName = Ext.getCmp("editQueryLoginName");
+    const editLoginName = PCL.getCmp("editQueryLoginName");
     if (editLoginName) {
       queryLoginName = editLoginName.getValue();
     }
 
     let queryName = null;
-    const editQueryName = Ext.getCmp("editQueryName");
+    const editQueryName = PCL.getCmp("editQueryName");
     if (editQueryName) {
       queryName = editQueryName.getValue();
     }
 
     let enabled = -1;
-    const edit = Ext.getCmp("editQueryEnabled");
+    const edit = PCL.getCmp("editQueryEnabled");
     if (edit) {
       enabled = edit.getValue();
     }
