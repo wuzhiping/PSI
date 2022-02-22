2283f2cbc94143385470d9f84a2ef63266fec9ef
CRM8000 <crm8000@qq.com>
2022-02-22 10:28:03 +0800

    FId一览

--- web/Application/Home/DAO/FIdListDAO.class.php
+++ web/Application/Home/DAO/FIdListDAO.class.php
@@ -21,6 +21,10 @@ class FIdListDAO extends PSIBaseExDAO
 
     $result = [];
 
+    // t_fid中的均为 系统固有和SLN0000
+    $category = "系统固有";
+    $sln = "SLN0000 - PSI低代码应用平台";
+
     $sql = "select fid, code, py, name from t_fid order by code";
     $data = $db->query($sql);
 
@@ -30,9 +34,49 @@ class FIdListDAO extends PSIBaseExDAO
         "code" => $v["code"],
         "name" => $v["name"],
         "py" => $v["py"],
-        "category" => "系统固有",
-        "sln" => "SLN0000 - PSI低代码应用平台",
+        "category" => $category,
+        "sln" => $sln,
       ];
+    }
+
+    // t_fid_plus 由码表设置、自定义表单、视图开发助手等模块添加的Fid
+    $sql = "select fid, code, py, name from t_fid_plus order by code";
+    $data = $db->query($sql);
+
+    foreach ($data as $v) {
+      $fid = $v["fid"];
+      $item = [
+        "fid" => $fid,
+        "code" => $v["code"],
+        "name" => $v["name"],
+        "py" => $v["py"],
+      ];
+
+      $category = "";
+      $sln = "";
+      if (substr($fid, 0, 2) == "ct") {
+        // 码表
+        $category = "码表";
+
+        $sql = "select s.name, m.sln_code
+                from t_code_table_md m, t_solution s
+                where m.sln_code = s.code and m.fid = '%s' ";
+        $d = $db->query($sql, $fid);
+        if ($d) {
+          $n = $d[0]["name"];
+          $c = $d[0]["sln_code"];
+          $sln = "{$c} - {$n}";
+        } else {
+          $sln = "未查询到解决方案";
+        }
+      } else {
+        // TODO 其他模块的查询逻辑
+      }
+
+      $item["category"] = $category;
+      $item["sln"] = $sln;
+
+      $result[] = $item;
     }
 
     return $result;
