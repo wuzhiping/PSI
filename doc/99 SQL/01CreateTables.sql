e7e9e9d05c884ef22961956868adb63e5d4ca1a4
CRM8000 <crm8000@qq.com>
2022-03-01 16:04:07 +0800

    更新数据库文档

--- doc/99 SQL/01CreateTables.sql
+++ doc/99 SQL/01CreateTables.sql
@@ -1836,6 +1836,30 @@ CREATE TABLE IF NOT EXISTS `t_sysdict_sln0000_user_enabled` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+DROP TABLE IF EXISTS `t_sysdict_sln0000_ct_tree_view`;
+CREATE TABLE IF NOT EXISTS `t_sysdict_sln0000_ct_tree_view` (
+  `id` varchar(255) NOT NULL,
+  `code` varchar(255) NOT NULL,
+  `code_int` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `py` varchar(255) NOT NULL,
+  `memo` varchar(255) NOT NULL,
+  `show_order` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `t_sysdict_sln0000_ct_view_paging`;
+CREATE TABLE IF NOT EXISTS `t_sysdict_sln0000_ct_view_paging` (
+  `id` varchar(255) NOT NULL,
+  `code` varchar(255) NOT NULL,
+  `code_int` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `py` varchar(255) NOT NULL,
+  `memo` varchar(255) NOT NULL,
+  `show_order` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
