93218eef64f8517d38c5947af6fc12faf5877ddf
CRM8000 <crm8000@qq.com>
2022-03-05 10:14:35 +0800

    关于 - 页面微调

--- web/Public/Scripts/PSI/About/MainForm.js
+++ web/Public/Scripts/PSI/About/MainForm.js
@@ -36,7 +36,7 @@ PCL.define("PSI.About.MainForm", {
         iconCls: "PSI-fid-9994",
         height: 40
       },
-      height: 420,
+      height: 430,
       bodyCls: "PSI-About-body",
       items: [{
         border: 0,
@@ -46,17 +46,20 @@ PCL.define("PSI.About.MainForm", {
         html: `
               <h1 style='color:#0050b3;margin-top:0px'>${me.getProductionName()}基于开源技术，提供人、财、物、产、供、销、存一体化的企业管理全面解决方案</h1>
               <hr color='#F2F6FC'/>
-              <p>当前版本：<span style='border-bottom:1px dashed #adc6ff'>${PSI.Const.VERSION}</span>
+              <p>
+                当前版本：<span style='border-bottom:1px dashed #adc6ff'>${PSI.Const.VERSION}</span>
+              </p>
+              <div style='margin-top:20px'></div>
               <p>
                 数据库结构版本号：<span style='border-bottom:1px dashed #adc6ff'>${me.getPSIDBVersion()}</span>
-                <span style='display:inline-block;width:50px'></span>
-                数据库结构更新时间：${me.getPSIDBUpdateDT()}
+                <span style='display:inline-block;width:60px'></span>
+                数据库结构更新时间：<span style='border-bottom:1px dashed #adc6ff'>${me.getPSIDBUpdateDT()}</span>
               </p>
               <p>UI组件PCL版本号：<span style='border-bottom:1px dashed #adc6ff'>${PCL.VERSION}</span></p>
               <p>
-                PHP版本号：<span style='border-bottom:1px dashed #adc6ff'>${me.getPhpVersion()}</span> , 
-                MySQL版本号：<span style='border-bottom:1px dashed #adc6ff'>${me.getMySQLVersion()}</span></p>
-              <div style='height:10px'></div>
+                PHP版本号：<span style='border-bottom:1px dashed #adc6ff'>${me.getPhpVersion()}</span>
+                &nbsp;&nbsp;MySQL版本号：<span style='border-bottom:1px dashed #adc6ff'>${me.getMySQLVersion()}</span></p>
+              <div style='height:30px'></div>
               <div style='border-left:3px solid #1890ff'>
                 <h3>&nbsp;&nbsp;官网&nbsp;<a style='text-decoration:none;' href='https://gitee.com/crm8000/PSI' target='_blank'>https://gitee.com/crm8000/PSI</a></h3>
                 <p>&nbsp;&nbsp;${c}</p>
@@ -65,7 +68,7 @@ PCL.define("PSI.About.MainForm", {
       }],
       buttons: [{
         id: "buttonAboutFormOK",
-        text: "关闭",
+        text: "返回首页",
         handler: me._onOK,
         scope: me,
         iconCls: "PSI-button-ok"
