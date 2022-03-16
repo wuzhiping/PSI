44ac82c0855f47c51ac63cfba9f1d9c966263a52
CRM8000 <crm8000@qq.com>
2022-03-16 10:37:47 +0800

    码表设置

---
+++ web/Public/Scripts/PSI/CodeTable/SelectColRefForm.js
@@ -0,0 +1,49 @@
+/**
+ * 值来源的引用列
+ * 
+ * @author 艾格林门信息服务（大连）有限公司
+ * @copyright 2015 - present
+ * @license GPL v3
+ */
+PCL.define("PSI.CodeTable.SelectColRefForm", {
+  extend: "PCL.window.Window",
+
+  mixins: ["PSI.AFX.Mix.Common"],
+
+  config: {
+    codeTable: null
+  },
+
+  initComponent() {
+    const me = this;
+
+    PCL.apply(me, {
+      header: {
+        title: me.formatTitle(PSI.Const.PROD_NAME),
+        height: 40
+      },
+      width: 800,
+      height: 420,
+      layout: "border",
+      buttons: [{
+        text: "关闭",
+        handler() {
+          me.close();
+        },
+        scope: me
+      }],
+      listeners: {
+        show: {
+          fn: me._onWndShow,
+          scope: me
+        }
+      }
+    });
+
+    me.callParent(arguments);
+  },
+
+  _onWndShow() {
+    const me = this;
+  }
+});
