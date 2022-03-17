7405f76348831803b9c003b45772aa5733053b70
CRM8000 <crm8000@qq.com>
2022-03-17 11:07:38 +0800

    物料 - 应用PCL

--- web/Public/Scripts/PSI/Goods/SubGoodsField.js
+++ web/Public/Scripts/PSI/Goods/SubGoodsField.js
@@ -5,8 +5,8 @@
  * @copyright 2015 - present
  * @license GPL v3
  */
-Ext.define("PSI.Goods.SubGoodsField", {
-  extend: "Ext.form.field.Trigger",
+PCL.define("PSI.Goods.SubGoodsField", {
+  extend: "PCL.form.field.Trigger",
   alias: "widget.psi_subgoodsfield",
 
   config: {
@@ -54,17 +54,17 @@ Ext.define("PSI.Goods.SubGoodsField", {
   onTriggerClick: function (e) {
     var me = this;
     var modelName = "PSISubGoodsField";
-    Ext.define(modelName, {
-      extend: "Ext.data.Model",
+    PCL.define(modelName, {
+      extend: "PCL.data.Model",
       fields: ["id", "code", "name", "spec", "unitName"]
     });
 
-    var store = Ext.create("Ext.data.Store", {
+    var store = PCL.create("PCL.data.Store", {
       model: modelName,
       autoLoad: false,
       data: []
     });
-    var lookupGrid = Ext.create("Ext.grid.Panel", {
+    var lookupGrid = PCL.create("PCL.grid.Panel", {
       cls: "PSI",
       columnLines: true,
       border: 1,
@@ -94,7 +94,7 @@ Ext.define("PSI.Goods.SubGoodsField", {
     me.lookupGrid = lookupGrid;
     me.lookupGrid.on("itemdblclick", me.onOK, me);
 
-    var wnd = Ext.create("Ext.window.Window", {
+    var wnd = PCL.create("PCL.window.Window", {
       title: "选择 - 子件",
       modal: true,
       width: 600,
@@ -144,10 +144,10 @@ Ext.define("PSI.Goods.SubGoodsField", {
     });
     me.wnd = wnd;
 
-    var editName = Ext.getCmp("__editGoods");
+    var editName = PCL.getCmp("__editGoods");
     editName.on("change", function () {
       var store = me.lookupGrid.getStore();
-      Ext.Ajax.request({
+      PCL.Ajax.request({
         url: PSI.Const.BASE_URL
           + "Home/Goods/queryDataForSubGoods",
         params: {
@@ -158,8 +158,7 @@ Ext.define("PSI.Goods.SubGoodsField", {
         callback: function (opt, success, response) {
           store.removeAll();
           if (success) {
-            var data = Ext.JSON
-              .decode(response.responseText);
+            var data = PCL.JSON.decode(response.responseText);
             store.add(data);
             if (data.length > 0) {
               me.lookupGrid.getSelectionModel().select(0);
