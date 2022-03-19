f414e5afabf178fd10c0b93691371f51b47cd581
CRM8000 <crm8000@qq.com>
2022-03-19 11:36:29 +0800

    码表设置

--- web/Public/Scripts/PSI/CodeTable/SelectColRefForm.js
+++ web/Public/Scripts/PSI/CodeTable/SelectColRefForm.js
@@ -26,11 +26,40 @@ PCL.define("PSI.CodeTable.SelectColRefForm", {
         title: me.formatTitle(PSI.Const.PROD_NAME),
         height: 40
       },
+      modal: true,
+      closable: false,
+      resizable: false,
+      onEsc: PCL.emptyFn,
       width: 800,
       height: 600,
       layout: "border",
+      items: [
+        {
+          region: "west",
+          width: "50%",
+          layout: "fit",
+          border: 0,
+          items: me.getTableGrid(),
+        },
+        {
+          region: "center",
+          layout: "border",
+          border: 0,
+          items: [
+            {
+              region: "north",
+              border: 0,
+              height: "50%"
+            },
+            {
+              region: "center",
+              border: 0,
+            }
+          ]
+        }
+      ],
       buttons: [{
-        text: "关闭",
+        text: "取消",
         handler() {
           me.close();
         },
@@ -52,5 +81,81 @@ PCL.define("PSI.CodeTable.SelectColRefForm", {
    */
   _onWndShow() {
     const me = this;
-  }
+  },
+
+  /**
+   * 显示表的Grid
+   * @private
+   */
+  getTableGrid() {
+    const me = this;
+
+    if (me._tableGrid) {
+      return me._tableGrid;
+    }
+
+    const modelName = "PSIModel.PSI.CodeTable.SelectColRefForm.TableModel";
+
+    PCL.define(modelName, {
+      extend: "PCL.data.Model",
+      fields: ["name"]
+    });
+
+    me._tableGrid = PCL.create("PCL.grid.Panel", {
+      cls: "PSI",
+      viewConfig: {
+        enableTextSelection: true
+      },
+      header: {
+        height: 30,
+        title: me.formatGridHeaderTitle("表")
+      },
+      columnLines: true,
+      columns: {
+        defaults: {
+          menuDisabled: true,
+          sortable: false,
+        },
+        items: [{
+          header: "表名",
+          dataIndex: "name",
+          flex: 1,
+        }]
+      },
+      store: PCL.create("PCL.data.Store", {
+        model: modelName,
+        autoLoad: false,
+        data: []
+      }),
+      listeners: {
+        select: {
+          fn: me._onTableGridSelect,
+          scope: me
+        },
+      }
+    });
+
+    return me._tableGrid;
+  },
+
+  /**
+   * 显示关联字段的Grid
+   * 
+   * @private
+   */
+  getColForKeyGrid() {
+
+  },
+
+  /**
+   * 显示展示字段的Grid
+   * 
+   * @private
+   */
+  getColForDisplayGrid() { },
+
+  /**
+   * @private
+   */
+  _onTableGridSelect() { },
 });
