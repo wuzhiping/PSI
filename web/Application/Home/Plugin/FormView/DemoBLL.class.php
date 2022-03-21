ba99e865e90484ddf286ed317f037cb139d40141
CRM8000 <crm8000@qq.com>
2022-03-21 15:29:39 +0800

    码表设置

--- web/Application/Home/Plugin/FormView/DemoBLL.class.php
+++ web/Application/Home/Plugin/FormView/DemoBLL.class.php
@@ -6,7 +6,9 @@ namespace Home\Plugin\FormView;
  * 视图的示例自定义业务逻辑处理class
  * BLL 是Business Logic Layer的缩写，推荐使用该后缀作为惯例
  *
- * @author 李静波
+ * @author 艾格林门信息服务（大连）有限公司
+ * @copyright 2015 - present
+ * @license GPL v3
  */
 class DemoBLL
 {
