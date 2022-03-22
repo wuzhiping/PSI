0c8489f7276ea6f0035e72e8a695fbd9edaa9720
CRM8000 <crm8000@qq.com>
2022-03-22 10:52:54 +0800

    关于 - 增加代码授权说明

--- web/Public/Scripts/PSI/About/MainForm.js
+++ web/Public/Scripts/PSI/About/MainForm.js
@@ -36,7 +36,7 @@ PCL.define("PSI.About.MainForm", {
         iconCls: "PSI-fid-9994",
         height: 40
       },
-      height: 430,
+      height: 500,
       bodyCls: "PSI-About-body",
       items: [{
         border: 0,
@@ -44,7 +44,7 @@ PCL.define("PSI.About.MainForm", {
         margin: "0 0 0 10",
         cls: "PSI-about",
         html: `
-              <h1 style='color:#0050b3;margin-top:0px'>${me.getProductionName()}基于开源技术，提供人、财、物、产、供、销、存一体化的企业管理全面解决方案</h1>
+              <h1 style='color:#0050b3;margin-top:0px;margin-left:-5px'>${me.getProductionName()}基于开源技术，提供人、财、物、产、供、销、存一体化的企业管理全面解决方案</h1>
               <div style='margin-left:-10px;border-bottom:1px solid #e6f7ff;height:1px' /></div>
               <p>
                 当前版本：<span style='border-bottom:1px solid #adc6ff'>${PSI.Const.VERSION}</span>
@@ -56,13 +56,17 @@ PCL.define("PSI.About.MainForm", {
                 数据库结构更新时间：<span style='border-bottom:1px solid #adc6ff'>${me.getPSIDBUpdateDT()}</span>
               </p>
               <div style='margin-top:20px'></div>
-              <p>UI组件PCL版本号：<span style='border-bottom:1px solid #adc6ff'>${PCL.VERSION}</span></p>
-              <div style='margin-top:20px'></div>
               <p>
-                PHP版本号：<span style='border-bottom:1px solid #adc6ff'>${me.getPhpVersion()}</span>
-                &nbsp;&nbsp;MySQL版本号：<span style='border-bottom:1px solid #adc6ff'>${me.getMySQLVersion()}</span></p>
-              <div style='height:30px'></div>
-              <div style='border-left:3px solid #1890ff'>
+                UI组件PCL版本号：<span style='border-bottom:1px solid #adc6ff'>${PCL.VERSION}</span>
+                &nbsp;&nbsp;PHP版本号：<span style='border-bottom:1px solid #adc6ff'>${me.getPhpVersion()}</span>
+                &nbsp;&nbsp;MySQL版本号：<span style='border-bottom:1px solid #adc6ff'>${me.getMySQLVersion()}</span>
+              </p>
+              <div style='margin-top:40px;border-left:3px solid #fa8c16'>
+                <h3>&nbsp;&nbsp;代码授权说明</h3>
+                <p>&nbsp;&nbsp;在遵守GPL开源协议的前提下，授权您可以把PSI的代码用于<span style='border-bottom:2px solid #d3adf7'>任何</span>您需要的商业用途</p>
+                <p>&nbsp;&nbsp;利用PSI的代码给您带来商业收益，这是对PSI最大的褒奖</p>
+              </div>
+              <div style='margin-top:40px;border-left:3px solid #1890ff'>
                 <h3>&nbsp;&nbsp;官网&nbsp;<a style='text-decoration:none;' href='https://gitee.com/crm8000/PSI' target='_blank'>https://gitee.com/crm8000/PSI</a></h3>
                 <p>&nbsp;&nbsp;${c}</p>
               </div>
