a5315988f613edd0cf6254e1ebe9f9f98106e1e3
CRM8000 <crm8000@qq.com>
2022-03-27 09:13:51 +0800

    更新版本号

--- web/Public/Scripts/PSI/Const.js
+++ web/Public/Scripts/PSI/Const.js
@@ -20,6 +20,6 @@ PCL.define("PSI.Const", {
     GC_DEC_NUMBER: 0, // 物料数量小数位数
     ENABLE_LODOP: "0", //是否启用Lodop打印
     PROD_NAME: "PSI", // 产品名称
-    VERSION: "PSI 2022 - built2022-03-22 17:55"
+    VERSION: "PSI 2022 - built2022-03-27 09:10"
   }
 });
